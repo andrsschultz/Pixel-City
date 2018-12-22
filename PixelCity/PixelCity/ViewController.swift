2bbdbce2fd6ae652e3ca56dcc5088d3935329e9b
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-22 10:59:20 +0100

    Project Creation

---
+++ PixelCity/PixelCity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Andreas Schultz on 22.12.18.
+//  Copyright © 2018 Andreas Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
